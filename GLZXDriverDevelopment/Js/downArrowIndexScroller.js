4db0102d567a060d483d99f3bc7a253e989f0f44
GLZX4 <122561008+GLZX4@users.noreply.github.com>
2023-03-19 01:09:48 +0000

    Add files via upload

--- GLZXDriverDevelopment/Js/downArrowIndexScroller.js
+++ GLZXDriverDevelopment/Js/downArrowIndexScroller.js
@@ -1,5 +1,6 @@
 //Code for scrolling down the page on image click adapted from: https://www.w3schools.com/jsref/met_win_scrollto.asp
 
+//added a DOMContentLoaded event listener to ensure the whole webpage has been loaded first.
 document.addEventListener('DOMContentLoaded', () => {
     const downArrow = document.querySelector('.downArrow');
   
